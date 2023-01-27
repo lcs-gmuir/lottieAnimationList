7872e29499e2141d841839ad0fc4121f3f745076
griffin muir <gmuir@lcs.on.ca>
2023-01-27 14:18:31 -0500

    Initial Commit

---
+++ lottieAnimationList/lottieAnimationListApp.swift
@@ -0,0 +1,17 @@
+//
+//  lottieAnimationListApp.swift
+//  lottieAnimationList
+//
+//  Created by Samantha Stewart on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct lottieAnimationListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
